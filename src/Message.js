7d7f9a13f63199a310678db452f40c88679ff058
Dan Meyer <dmeyf@allstate.com>
2017-08-20 22:17:05 -0500

    Select message complete

--- src/Message.js
+++ src/Message.js
@@ -2,24 +2,33 @@ import React, {Component} from 'react'
 
 class Message extends Component {
 
-    state = {starred: this.props.message.starred}
+    state = {
+        starred: this.props.message.starred,
+        selected: this.props.message.selected,
+    }
 
     starHandler = () => {
         this.setState({starred: !this.state.starred})
     }
 
+    selectHandler = () => {
+        this.setState({selected: !this.state.selected})
+    }
+
     render() {
         return (
             <div className={"row message " + (this.props.message.read ? "read " : "unread ")
-            + (this.props.message.selected ? "selected" : "")}>
+            + (this.state.selected ? "selected" : "")}>
                 <div className="col-xs-1">
                     <div className="row">
                         <div className="col-xs-2">
-                            <input type="checkbox" checked={this.props.message.selected}/>
+                            <input type="checkbox" defaultChecked={this.state.selected}
+                                   onChange={this.selectHandler}
+                            />
                         </div>
                         <div className="col-xs-2">
                             <i className={"star fa " + (this.state.starred ? "fa-star" : "fa-star-o")}
-                            onClick={this.starHandler}></i>
+                               onClick={this.starHandler}></i>
                         </div>
                     </div>
                 </div>
